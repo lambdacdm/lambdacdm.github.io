8bf353acc6ae8494617dba0659fd4b59d88fbe62
lambdacdm <lixiang-qd@163.com>
2024-07-01 17:36:19 +0800

    newpretext

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,302 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "xiangli-2",
+  "level": "1",
+  "url": "xiangli-2.html",
+  "type": "Front Matter",
+  "number": "",
+  "title": "Front Matter",
+  "body": "   李想    "
+},
+{
+  "id": "home",
+  "level": "1",
+  "url": "home.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Home",
+  "body": " Home  Welcome to my personal website! Here is the definition of me.  I'm Xiang Li 李想 , a first year math PhD student of algebra and number theory in the University of Edinburgh working with Prof. Minhyong Kim . My interests are in algebraic number theory and arithmetic geometry. To be more specific, I am interested in non-abelian Chabauty methods, which provide an efficient way to find rational points on a curve.   Am I the first person coming up with an idea to make the personal website look like a math book that contains definition\/remark etc.?   Contact me via s2132101@ed.ac.uk . And click here to see my CV (curriculum vitae). For more infos, click buttons on the sidebar.      The commutative diagram is called the Kim's cutter, which is Prof.Minhyong Kim's work and I'm trying to understand it.    The cursed curve, plotted using SageMath by Sachi Hashimoto    "
+},
+{
+  "id": "home-3",
+  "level": "2",
+  "url": "home.html#home-3",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "",
+  "body": "I'm Xiang Li 李想 , a first year math PhD student of algebra and number theory in the University of Edinburgh working with Prof. Minhyong Kim . My interests are in algebraic number theory and arithmetic geometry. To be more specific, I am interested in non-abelian Chabauty methods, which provide an efficient way to find rational points on a curve. "
+},
+{
+  "id": "home-5",
+  "level": "2",
+  "url": "home.html#home-5",
+  "type": "Remark",
+  "number": "1.1.2",
+  "title": "",
+  "body": "Contact me via s2132101@ed.ac.uk . And click here to see my CV (curriculum vitae). For more infos, click buttons on the sidebar. "
+},
+{
+  "id": "fig-X13",
+  "level": "2",
+  "url": "home.html#fig-X13",
+  "type": "Figure",
+  "number": "1.1.3",
+  "title": "",
+  "body": " The cursed curve, plotted using SageMath by Sachi Hashimoto   "
+},
+{
+  "id": "contact",
+  "level": "1",
+  "url": "contact.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Contact",
+  "body": " Contact    I can be contacted in the following ways:  University Email: s2132101@ed.ac.uk  Personal Email: lxqd2021@outlook.com  Github: lambdacdm  Discord\/Steam\/Spotify: lxqd2021  知乎\/b站\/网易云音乐: 寨森CDM       Just contact me if you wish!   In workdays, you can find me on the fifth floor of the Bayes Centre of the University of Edinburgh. The address of the Bayes Centre is 47 Potterrow, Edinburgh, United Kingdom, EH8 9BT (or find the address in Google map by clicking here ).  "
+},
+{
+  "id": "thm-",
+  "level": "2",
+  "url": "contact.html#thm-",
+  "type": "Proposition",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  I can be contacted in the following ways:  University Email: s2132101@ed.ac.uk  Personal Email: lxqd2021@outlook.com  Github: lambdacdm  Discord\/Steam\/Spotify: lxqd2021  知乎\/b站\/网易云音乐: 寨森CDM     "
+},
+{
+  "id": "contact-3",
+  "level": "2",
+  "url": "contact.html#contact-3",
+  "type": "Proof",
+  "number": "1.2.1",
+  "title": "",
+  "body": " Just contact me if you wish!  "
+},
+{
+  "id": "contact-4",
+  "level": "2",
+  "url": "contact.html#contact-4",
+  "type": "Remark",
+  "number": "1.2.2",
+  "title": "",
+  "body": "In workdays, you can find me on the fifth floor of the Bayes Centre of the University of Edinburgh. The address of the Bayes Centre is 47 Potterrow, Edinburgh, United Kingdom, EH8 9BT (or find the address in Google map by clicking here ). "
+},
+{
+  "id": "cv",
+  "level": "1",
+  "url": "cv.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "CV",
+  "body": " CV   My CV (curriculum vitae) can be found by clicking here .    Educational Background     Sep 2023 - Now (PhD) The University of Edinburgh School of Mathematics (Hodge Institute)     Oct 2022 - Jun 2023 (MASt) University of Cambridge Department of Pure Mathematics and Mathematical Statistics     Sep 2020 - May 2022 (BSc) The University of Edinburgh School of Mathematics     Sep 2018 - Jun 2020 (BSc) South China University of Technology School of Mathematics      Recent Activities (Conferences)  I have attended\/will attend the following conferences.     Gauge Fields in Arithmetic, Topology and Physics 15-19 Apr 2024, ICMS, Edinburgh, UK       Winter Workshop Chabauty-Kim 2024 14-16 Feb 2024, Heidelberg University, Germany     "
+},
+{
+  "id": "cv-2",
+  "level": "2",
+  "url": "cv.html#cv-2",
+  "type": "Lemma",
+  "number": "1.3.1",
+  "title": "",
+  "body": " My CV (curriculum vitae) can be found by clicking here .  "
+},
+{
+  "id": "subsecedu-2-1-1",
+  "level": "2",
+  "url": "cv.html#subsecedu-2-1-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Sep 2023 - Now (PhD) The University of Edinburgh "
+},
+{
+  "id": "subsecedu-2-2-1",
+  "level": "2",
+  "url": "cv.html#subsecedu-2-2-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Oct 2022 - Jun 2023 (MASt) University of Cambridge "
+},
+{
+  "id": "subsecedu-2-3-1",
+  "level": "2",
+  "url": "cv.html#subsecedu-2-3-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Sep 2020 - May 2022 (BSc) The University of Edinburgh "
+},
+{
+  "id": "subsecedu-2-4-1",
+  "level": "2",
+  "url": "cv.html#subsecedu-2-4-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Sep 2018 - Jun 2020 (BSc) South China University of Technology "
+},
+{
+  "id": "subsec-conference-3-1-1",
+  "level": "2",
+  "url": "cv.html#subsec-conference-3-1-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Gauge Fields in Arithmetic, Topology and Physics "
+},
+{
+  "id": "subsec-conference-4-1-1",
+  "level": "2",
+  "url": "cv.html#subsec-conference-4-1-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Winter Workshop Chabauty-Kim 2024 "
+},
+{
+  "id": "writing",
+  "level": "1",
+  "url": "writing.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Writing",
+  "body": " Writing   Reports of Projects  Here lists my reports of some projects:     Tensor Category in Arithmetic, Geometry and Physics (Dec 2023, GlaMS Group Project, with Julia Bierent, Gianni Gagliardo, Chun-Yu Bai)    The Modularity Theorem and the Modular Approach (May 2023, Part III Essay)    Net of Conics and Cubic Curves (Mar 2022, Undergraduate Final Year Project, with Yasmine Bligaard, Saffron Wang, Ruby David-Jekyll)    Algebra Theorem Proving in Lean (Sep 2021, Undergraduate Summer Project)       Notes  Here are some of my notes (very informal).     Chabauty-Kim Methods     Hodge Structure  (with minor mistake)    Affine Group Scheme over a Field  (manuscript)    Dictionary of Algebraic Geometry    Root System    实变函数笔记 (Notes on Real Variables) (written in Chinese)    Riemann Surface (written in Chinese)     Also, you can see my posts on Zhihu 知乎 (written in Chinese).   "
+},
+{
+  "id": "writing-2-3-1-1-1",
+  "level": "2",
+  "url": "writing.html#writing-2-3-1-1-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Tensor Category in Arithmetic, Geometry and Physics "
+},
+{
+  "id": "writing-2-3-1-2-1",
+  "level": "2",
+  "url": "writing.html#writing-2-3-1-2-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "The Modularity Theorem and the Modular Approach "
+},
+{
+  "id": "writing-2-3-1-3-1",
+  "level": "2",
+  "url": "writing.html#writing-2-3-1-3-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Net of Conics and Cubic Curves "
+},
+{
+  "id": "writing-2-3-1-4-1",
+  "level": "2",
+  "url": "writing.html#writing-2-3-1-4-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Algebra Theorem Proving in Lean "
+},
+{
+  "id": "writing-3-3-1-1-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-1-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Chabauty-Kim Methods "
+},
+{
+  "id": "writing-3-3-1-2-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-2-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Hodge Structure  "
+},
+{
+  "id": "writing-3-3-1-3-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-3-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Affine Group Scheme over a Field  "
+},
+{
+  "id": "writing-3-3-1-4-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-4-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Dictionary of Algebraic Geometry "
+},
+{
+  "id": "writing-3-3-1-5-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-5-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Root System "
+},
+{
+  "id": "writing-3-3-1-6-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-6-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "实变函数笔记 (Notes on Real Variables) "
+},
+{
+  "id": "writing-3-3-1-7-1",
+  "level": "2",
+  "url": "writing.html#writing-3-3-1-7-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Riemann Surface "
+},
+{
+  "id": "talks",
+  "level": "1",
+  "url": "talks.html",
+  "type": "Section",
+  "number": "1.5",
+  "title": "Talks",
+  "body": " Talks   Upcoming Talks  There is no upcoming talk.    Past Talks    Tannakian Category, Unipotent Completion, and de Rham Fundamental Group   1 Dec 2023, 10:45-11:15, ICMS     It's an Examples Showcase in GlaMS .        Euler's Totient Theorem and the Prime Number Theorem (with Ella Yu)  16 Feb 2023, 16:00-17:00, Zoom\/Huxley 410 Imperial College  It's a seminar of London Learning Lean . Watch the recording here .     "
+},
+{
+  "id": "teaching",
+  "level": "1",
+  "url": "teaching.html",
+  "type": "Section",
+  "number": "1.6",
+  "title": "Teaching",
+  "body": " Teaching   2023-2024 Semester 2  I'm currently tutoring two courses.    [ MATH08058 ] Calculus and its Applications    [ MATH10077 ] Algebraic Topology     "
+},
+{
+  "id": "codes",
+  "level": "1",
+  "url": "codes.html",
+  "type": "Section",
+  "number": "1.7",
+  "title": "Codes",
+  "body": " Codes    LEAN is a proof assistant, which allows people to write the proof of theorems in a computer programme language. See the website of LEAN community .  A team of me, Huajian Xin, Ella Yu and others attempted to prove the prime number theorem on LEAN. See our Github repository . Ella Yu and I gave a talk about this project on a seminar of London Learning Lean held by Imperial College. Watch the recording here .    Numerical Library of Mathematics Algorithm  I made a simple C++ library for numerical algorithms such as solving linear systems, integrals, differential equations and so on numerically. See my Github repository .   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
